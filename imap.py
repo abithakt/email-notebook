f4ea62c249bdbc5bbcd18e689dd7f50554732872
Abitha <abithathyagarajan@gmail.com>
2018-01-28 14:42:40 +0530

    Update imap.py

--- imap.py
+++ imap.py
@@ -4,6 +4,7 @@ import ruamel.yaml as yaml
 with open("config.yml", 'r') as ymlfile:
 	config = yaml.safe_load(ymlfile)
 
-print(config['gmail']['imap_server'])
+#print(config['gmail']['imap_server'])
 
-#mail = imaplib.IMAP4_SSL(config['gmail'])
+mail = imaplib.IMAP4_SSL(config['gmail']['imap_server'])
+mail.login(config['email'], config['password'])
