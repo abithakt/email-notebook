455b0026e5db6de99a019a543e57cd0160a75888
Abitha K Thyagarajan <abithathyagarajan@gmail.com>
2018-01-31 09:55:48 +0530

    Update imap2.py

--- imap2.py
+++ imap2.py
@@ -3,13 +3,24 @@ import ruamel.yaml as yaml
 from imapclient import IMAPClient as imapc
 from datetime import datetime
 
+
+
 # Open config file
 with open('config.yml', 'r+') as ymlfile:
     config = yaml.safe_load(ymlfile)
     
+
+    
 # Record time of last access - TODO
 last_accessed = datetime.now()
 
-# Log in
 
+
+# Log in
 mail = imapc(config['gmail']['imap_server'], use_uid = True)
+rv = mail.login(config['email'], config['password'])
+
+
+
+# List mailboxes
+#mailboxes = 
