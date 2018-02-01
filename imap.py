0710e37fc9ee811128429f09da85fe5a09d251ca
Abitha <abithathyagarajan@gmail.com>
2018-02-01 10:24:10 +0530

    Update imap2.py

--- imap.py
+++ imap.py
@@ -1,10 +1,80 @@
-import imaplib
+import imaplib, email
 import ruamel.yaml as yaml
 
+# Open config file
 with open("config.yml", 'r') as ymlfile:
 	config = yaml.safe_load(ymlfile)
 
-#print(config['gmail']['imap_server'])
+#if "gmail.com"
 
+# Log in
 mail = imaplib.IMAP4_SSL(config['gmail']['imap_server'])
 mail.login(config['email'], config['password'])
+#mail.select('inbox')
+
+# List mailboxes
+rv, mailboxes = mail.list()
+
+# Find the name of the 'All Mail' folder
+all = str("")
+for i in mailboxes:
+    if "\All" in str(i, 'utf-8'):
+        a = str(i, 'utf-8')
+        all = a[a.index('['):a.index('"', -1)]
+all = '"' + all
+all = all + '"'
+
+# Open 'All Mail'
+rv, data = mail.select(all)
+
+#------------------------------------------------------------------------------
+
+# Fetch all emails in 'All Mail'
+rv, data = mail.search(None, "ALL")
+print(data[0])
+
+a = """for i in str(data[0], 'utf-8').split(): # data[0].split() gets email IDs
+    rv, data = mail.fetch(i, '(RFC822)')
+    msg = email.message_from_string(data[0][1])
+    print( 'Message %s: %s' % (num, msg['Subject']))
+    print('Raw Date:', msg['Date'])
+    date_tuple = email.utils.parsedate_tz(msg['Date'])
+    if date_tuple:
+        local_date = datetime.datetime.fromtimestamp(
+            email.utils.mktime_tz(date_tuple))
+        print( "Local Date:", \
+            local_date.strftime("%a, %d %b %Y %H:%M:%S"))
+"""
+
+for num in data[0].split():
+    typ, data = mail.fetch(num, '(RFC822)')
+    print('Message %s\n%s\n' % (num, data[0][1]))
+
+b = """ .
+def fetch_message(conn, msg_uid ):
+
+    rv, data = conn.uid('fetch', msg_uid, "(RFC822)")
+    if rv != 'OK':
+        print "ERROR fetching message #", msg_uid
+        return {}
+
+    return email.message_from_string(data[0][1])  # dict-like object
+"""
+c = """ 
+def get_recipients(msg_parsed):
+    recipients = []
+    addr_fields = ['From', 'To', 'Cc', 'Bcc']
+
+    for f in addr_fields:
+        rfield = msg_parsed.get(f, "") # Empty string if field not present
+        rlist = re.findall(ADDR_PATTERN, rfield)
+        recipients.extend(rlist)
+
+return recipients
+"""
+
+# Store most recent email's timestamp in config file
+
+# log out
+mail.close()
+mail.logout()
---
+++ imap2.py
@@ -0,0 +1,89 @@
+import email, logging, mistletoe
+import ruamel.yaml as yaml
+from imapclient import IMAPClient as imapc
+from datetime import datetime
+
+
+
+# Open config file
+
+with open("config.yml", 'r') as ymlfile:
+	config = yaml.safe_load(ymlfile)
+
+
+
+#if "gmail.com"
+#last accessed...
+
+last_accessed = datetime.now()
+
+
+# Log in
+
+mail = imapc(config['gmail']['imap_server'], use_uid = True)
+rv = mail.login(config['email'], config['password'])
+
+
+
+# List mailboxes
+
+mailboxes = mail.list_folders()
+mailboxes = [str(x) for x in mailboxes]
+
+
+
+# Find the name of the 'All Mail' folder
+
+all = str("")
+for a in mailboxes:
+    if "\All" in a:
+        all = a[a.index('['):a.index(')', -2)]
+
+all = all[:-1]
+
+
+
+# Open 'All Mail'
+
+allmail = mail.select_folder(all)
+
+
+
+# Identify messages from self/custom 'from' address
+
+from_messages = mail.search(['FROM', config['from']])
+
+# from_messages = mail.search(['FROM', config['from'], 'SINCE', config['last_accessed'], 'SUBJECT', config['flags']['wiki']])
+
+print(from_messages)
+
+#----------------------------------------------------------------------------------
+
+
+
+# Identify messages with '[wiki]'/custom flag
+
+wikipage = []
+for msgid, data in mail.fetch(from_messages, ['ENVELOPE']).items():
+    envelope = data[b'ENVELOPE']
+    #print(envelope.from_)
+    if str(config['flags']['wiki']) in str(envelope.subject): wikipage.append(msgid)
+    #print("ID \#%d: '%s' received %s" % (msgid, envelope.subject, envelope.date))
+
+
+# Get email body
+
+#messages = mail.search(['NOT DELETED'])
+response = mail.fetch(from_messages, ['RFC822', 'BODY[TEXT]'])
+print(data)
+for msgid, data in response.items():
+        parsedEmail = email.message_from_string(data['RFC822'])
+        body = email.message_from_string(data['BODY[TEXT]'])
+        parsedBody = parsedEmail.get_payload(0)
+        print(parsedBody)
+
+
+# Close folder and log out
+
+mail.close_folder()
+mail.logout()
