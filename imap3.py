3683c95b478bc2090d7e83d982aeaf3bdd8cba22
Abitha <abithathyagarajan@gmail.com>
2018-02-02 07:17:08 +0530

    Update imap3.py

--- imap3.py
+++ imap3.py
@@ -38,7 +38,6 @@ all = str("")
 for a in mailboxes:
     if "\All" in a:
         all = a[a.index('['):a.index(')', -2)]
-
 all = all[:-1]
 
 
@@ -51,8 +50,8 @@ allmail = mail.select_folder(all)
 
 # Identify messages from self/custom 'from' address
 
-from_messages = mail.search(['FROM', config['from']], 'SUBJECT', config['flags']['wiki']]))
-
+from_messages = mail.search(['FROM', config['from']]) #?
+# from_messages = mail.search(['FROM', config['from'], 'SUBJECT', 'm'])
 # from_messages = mail.search(['FROM', config['from'], 'SINCE', config['last_accessed'], 'SUBJECT', config['flags']['wiki']])
 
 print(from_messages)
@@ -64,9 +63,9 @@ print(from_messages)
 # Get email bodies
 wikipages = mail.fetch(from_messages, ['BODY[]', 'FLAGS']).items()
 print(wikipages)
-for k in wikipages:
-    message = pyzmail.PyzMessage.factory(k['BODY[]'])
-    print(message.text_part.get_payload().decode(message.text_part.charset))
+"""for k in wikipages:
+    message = pyzmail.PyzMessage.factory(k['BODY[]']) #??
+    print(message.text_part.get_payload().decode(message.text_part.charset))"""
 
 # Close folder and log out
 
