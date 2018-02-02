b4b74b5e2ef08055ee3be6e9d543b4c93e2c4431
Abitha <abithathyagarajan@gmail.com>
2018-02-02 07:21:13 +0530

    Update imap3.py

--- imap3.py
+++ imap3.py
@@ -50,8 +50,8 @@ allmail = mail.select_folder(all)
 
 # Identify messages from self/custom 'from' address
 
-from_messages = mail.search(['FROM', config['from']]) #?
-# from_messages = mail.search(['FROM', config['from'], 'SUBJECT', 'm'])
+# from_messages = mail.search(['FROM', config['from']]) #?
+from_messages = mail.search(['FROM ' + config['from'] + ' SUBJECT m'])
 # from_messages = mail.search(['FROM', config['from'], 'SINCE', config['last_accessed'], 'SUBJECT', config['flags']['wiki']])
 
 print(from_messages)
